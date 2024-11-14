e69e1042c9756b716acff78f14176bfdadaed9c6
John Oliver <1615532+johnoliver@users.noreply.github.com>
2024-11-14 16:29:40 +0000

    Fix sha265 typo

--- adoptium-marketplace-schema/src/main/java/net/adoptium/marketplace/schema/Asset.java
+++ adoptium-marketplace-schema/src/main/java/net/adoptium/marketplace/schema/Asset.java
@@ -8,7 +8,13 @@ public class Asset {
 
     public static final String SHA_256_SUM_LINK_NAME = "sha256sum_link";
     public static final String SIGNATURE_LINK_NAME = "signature_link";
-    public static final String SHA256SUM_NAME = "sha265sum";
+
+
+    public static final String SHA256SUM_NAME = "sha256sum";
+
+    // Name of previous typo to be removed when vendors update their code to fix the typo
+    public static final String SHA265SUM_NAME = "sha265sum";
+
     @Schema(example = "OpenJDK8U-jre_x86-32_windows_hotspot_8u212b04.msi", required = true)
     private final String name;
 
@@ -19,27 +25,46 @@ public class Asset {
     private final String sha256sum;
 
     @Schema(
-        example = "https://github.com/AdoptOpenJDK/openjdk8-openj9-releases/ga/download/jdk8u162-b12_openj9-0.8.0/OpenJDK8-OPENJ9_x64_Linux_jdk8u162-b12_openj9-0.8.0.tar.gz.sha256.txt",
-        name = SHA_256_SUM_LINK_NAME
+            example = "dd28d6d2cde2b931caf94ac2422a2ad082ea62f0beee3bf7057317c53093de93",
+            description = "This value is present to avoid an API breaking change from a previous typo for sha256sum in the API. DO NOT USE THIS, it is deprecated and will be removed.",
+            deprecated = true)
+    private final String sha265sum;
+
+    @Schema(
+            example = "https://github.com/AdoptOpenJDK/openjdk8-openj9-releases/ga/download/jdk8u162-b12_openj9-0.8.0/OpenJDK8-OPENJ9_x64_Linux_jdk8u162-b12_openj9-0.8.0.tar.gz.sha256.txt",
+            name = SHA_256_SUM_LINK_NAME
     )
     private final String sha256sumLink;
 
     @Schema(
-        example = "https://github.com/AdoptOpenJDK/openjdk11-upstream-binaries/releases/download/jdk-11.0.5%2B10/OpenJDK11U-jdk_x64_linux_11.0.5_10.tar.gz.sign",
-        name = SIGNATURE_LINK_NAME
+            example = "https://github.com/AdoptOpenJDK/openjdk11-upstream-binaries/releases/download/jdk-11.0.5%2B10/OpenJDK11U-jdk_x64_linux_11.0.5_10.tar.gz.sign",
+            name = SIGNATURE_LINK_NAME
     )
     private final String signatureLink;
 
     @JsonCreator
     public Asset(
-        @JsonProperty("name") String name,
-        @JsonProperty("link") String link,
-        @JsonProperty(SHA256SUM_NAME) String sha256sum,
-        @JsonProperty(SHA_256_SUM_LINK_NAME) String sha256sumLink,
-        @JsonProperty(SIGNATURE_LINK_NAME) String signatureLink) {
+            @JsonProperty("name") String name,
+            @JsonProperty("link") String link,
+            @JsonProperty(SHA256SUM_NAME) String sha256sum,
+
+            // TODO: Remove this when vendors update their code to fix the typo
+            @JsonProperty(SHA265SUM_NAME) String sha265sum,
+
+            @JsonProperty(SHA_256_SUM_LINK_NAME) String sha256sumLink,
+            @JsonProperty(SIGNATURE_LINK_NAME) String signatureLink) {
         this.name = name;
         this.link = link;
-        this.sha256sum = sha256sum;
+
+        // TODO: Remove this when vendors update their code to fix the typo
+        if (sha256sum != null) {
+            this.sha256sum = sha256sum;
+            this.sha265sum = sha256sum;
+        } else {
+            this.sha256sum = sha265sum;
+            this.sha265sum = sha265sum;
+        }
+
         this.sha256sumLink = sha256sumLink;
         this.signatureLink = signatureLink;
     }
@@ -55,6 +80,11 @@ public class Asset {
     @JsonProperty(SHA256SUM_NAME)
     public String getSha256sum() {
         return sha256sum;
+    }
+
+    @JsonProperty(SHA265SUM_NAME)
+    public String getSha265sum() {
+        return sha265sum;
     }
 
     @JsonProperty(SHA_256_SUM_LINK_NAME)
--- adoptium-marketplace-schema/src/main/java/net/adoptium/marketplace/schema/Package.java
+++ adoptium-marketplace-schema/src/main/java/net/adoptium/marketplace/schema/Package.java
@@ -6,14 +6,25 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Package extends Asset {
 
+    public Package(
+            String name,
+            String link,
+            String sha256sum,
+            String sha256sumLink,
+            String signatureLink
+    ) {
+        super(name, link, sha256sum, sha256sum, sha256sumLink, signatureLink);
+    }
+
     @JsonCreator
     public Package(
-        @JsonProperty("name") String name,
-        @JsonProperty("link") String link,
-        @JsonProperty(Asset.SHA256SUM_NAME) String sha256sum,
-        @JsonProperty(Asset.SHA_256_SUM_LINK_NAME) String sha256sumLink,
-        @JsonProperty(Asset.SIGNATURE_LINK_NAME) String signatureLink
+            @JsonProperty("name") String name,
+            @JsonProperty("link") String link,
+            @JsonProperty(Asset.SHA256SUM_NAME) String sha256sum,
+            @JsonProperty(Asset.SHA265SUM_NAME) String sha265sum,
+            @JsonProperty(Asset.SHA_256_SUM_LINK_NAME) String sha256sumLink,
+            @JsonProperty(Asset.SIGNATURE_LINK_NAME) String signatureLink
     ) {
-        super(name, link, sha256sum, sha256sumLink, signatureLink);
+        super(name, link, sha256sum, sha265sum, sha256sumLink, signatureLink);
     }
 }
